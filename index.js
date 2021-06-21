ae00dc0ffb3acfe2946feefdeabc3e5af47c5786
LabGuy94 <labdev@bbox3.com>
2021-06-21 16:09:26 -0700

    Added basic Discord functions

---
+++ index.js
@@ -0,0 +1,38 @@
+const discord = require("discord.js")
+const noblox = require("noblox.js")
+require('dotenv').config();
+const token = process.env.DISCORDTOKEN
+const prefix = process.env.PREFIX
+const client = new discord.Client()
+
+const fs = require('fs')
+
+
+
+client.commands = new discord.Collection();
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log("ready");
+})
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args)
+    }
+})
+
+
+
+client.login(token)
